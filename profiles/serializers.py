d4f2644a9f3ad25e00ba7df7096eb4fce6c2c25b
andreagorsche <andrea.gorsche@gmail.com>
2023-05-16 19:13:34 +0000

    Add login and user permissions

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,15 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     chef = serializers.ReadOnlyField(source='chef.username')
+    is_chef = serializers.SerializerMethodField()
+
+    def get_is_chef (self,obj):
+        request = self.context['request']
+        return request.user == obj.chef
 
     class Meta:
         model = Profile
         fields = [
             'id', 'chef', 'image', 'created_at', 'updated_at', 'favorite_cuisine',
-            'bio'
+            'bio', 'is_chef'
         ]
