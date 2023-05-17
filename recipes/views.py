16993d7b17a7d6176a17f0e4f602ee89ee5bc29c
andreagorsche <andrea.gorsche@gmail.com>
2023-05-17 17:23:23 +0000

    Create post method for recipes in views

--- recipes/views.py
+++ recipes/views.py
@@ -12,3 +12,16 @@ class RecipeList(APIView):
             posts, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RecipeSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(chef=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
