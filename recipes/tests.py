b3fb476be581c7b226e0ed94f1bf418af211b17b
andreagorsche <andrea.gorsche@gmail.com>
2023-05-20 20:24:15 +0000

    Create test for loggedin user able to create recipe and not logged in user can't create recipe

--- recipes/tests.py
+++ recipes/tests.py
@@ -8,9 +8,19 @@ class PostListViewTests(APITestCase):
     def setUp(self):
         User.objects.create_user(username='andrea', password='rookiecoder')
 
-    def test_can_list_posts(self):
+    def test_can_list_recipes(self):
         andrea = User.objects.get(username='andrea')
         Recipe.objects.create(chef=andrea, title='recipe title')
         response = self.client.get('/recipes/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     
+    def test_logged_in_user_can_create_recipe(self):
+        self.client.login(username='andrea', password='rookiecoder')
+        response = self.client.post('/recipes/', {'title': 'recipe title'})
+        count = Recipe.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_user_not_logged_in_cant_create_recipe(self):
+        response = self.client.post('/recipes/', {'title': 'recipe title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
