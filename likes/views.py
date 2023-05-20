f79d9272c5ee58282c7ee90bc9d30f79484241a7
andreagorsche <andrea.gorsche@gmail.com>
2023-05-20 19:39:23 +0000

    Create Like generic views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from cooking_api.permissions import IsChefOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in.
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(chef=self.request.user)
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    permission_classes = [IsChefOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
