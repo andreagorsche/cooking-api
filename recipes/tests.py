b978380baff7bc7a7e9d7d2d5d34a125567db83d
andreagorsche <andrea.gorsche@gmail.com>
2023-05-20 19:55:41 +0000

    Created test if user can list recipes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Recipe
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='andrea', password='rookiecoder')
+
+    def test_can_list_posts(self):
+        andrea = User.objects.get(username='andrea')
+        Recipe.objects.create(chef=andrea, title='recipe title')
+        response = self.client.get('/recipes/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
