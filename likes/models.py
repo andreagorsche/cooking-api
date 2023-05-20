a842b7b5234640a787d1e41abf284b25658bb870
andreagorsche <andrea.gorsche@gmail.com>
2023-05-20 08:26:47 +0000

    Create model Like and migrate

---
+++ likes/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from recipes.models import Recipe
+
+
+class Like(models.Model):
+    """
+    Comment model, related to User and Post
+    """
+    chef = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = [['chef', 'recipe']]
+
+    def __str__(self):
+         return f'{self.chef} {self.recipe}'
+
