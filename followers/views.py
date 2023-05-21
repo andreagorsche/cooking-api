cfdad75381305f86dd516f22de15c441b89c149f
andreagorsche <andrea.gorsche@gmail.com>
2023-05-21 18:22:07 +0000

    Create Generic Views for Followers App

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from cooking_api.permissions import IsChefOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers, if logged in follow other users
+    """
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(follower=self.request.user)
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    If logged in follow and unfollow a chef
+    """
+    permission_classes = [IsChefOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
