a76ec7cd001e735b1956d4b4f8aa88a628f38aaa
andreagorsche <andrea.gorsche@gmail.com>
2023-05-21 18:48:03 +0000

    Refractoring Profile and Recipe views to generic views

--- profiles/views.py
+++ profiles/views.py
@@ -1,44 +1,20 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from profiles.models import Profile
 from profiles.serializers import ProfileSerializer
 from cooking_api.permissions import IsChefOrReadOnly
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-            )
-        return Response(serializer.data)
 
-    
-
-class ProfileDetail(APIView):
+class ProfileList(generics.ListCreateAPIView):
+    """
+    List all profiles
+    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsChefOrReadOnly]
 
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-            )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProfileDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve, update and delete a profile
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
--- recipes/views.py
+++ recipes/views.py
@@ -1,45 +1,23 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 from cooking_api.permissions import IsChefOrReadOnly
 
-class RecipeList(APIView):
+class RecipeList(generics.ListCreateAPIView):
+    """
+    List all recipes, if logged create your own recipe
+    """
     serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Recipe.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(recipe=self.request.user)
+
+class RecipeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve recipe, if logged in update and delete your own recipe
+    """
     permission_classes = [IsChefOrReadOnly]
-
-    def get(self, request):
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(
-            recipes, many=True, context={'request': request}
-            )
-        return Response(serializer.data)
-
-class RecipeDetail(APIView):
     serializer_class = RecipeSerializer
-    permission_classes = [IsChefOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            recipe = Recipe.objects.get(pk=pk)
-            self.check_object_permissions(self.request, recipe)
-            return recipe
-        except Recipe.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(
-            recipe, context={'request': request}
-            )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(recipe, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Recipe.objects.all()
