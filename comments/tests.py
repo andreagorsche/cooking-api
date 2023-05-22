21edb8ae3bc4119384e8bdf64a1c7924e9db0083
andreagorsche <andrea.gorsche@gmail.com>
2023-05-22 21:13:22 +0000

    Add tests to profiles

--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Comment
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CommentListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='andrea', password='rookiecoder')
+
+    def test_can_list_comments(self):
+        andrea = User.objects.get(username='andrea')
+        Comment.objects.create(chef=andrea, content='andreas comment')
+        response = self.client.get('/comments/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_logged_in_user_can_create_comment(self):
+        self.client.login(username='andrea', password='rookiecoder')
+        response = self.client.post('/comments/', {'content': 'andreas comment'})
+        count = Comment.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_user_not_logged_in_cant_create_comment(self):
+        response = self.client.post('/comments/', {'content': 'andreas comment'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProfileListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='andrea', password='rookiecoder')
+
+    def test_can_list_profiles(self):
+        andrea = User.objects.get(username='andrea')
+        Profile.objects.create(chef=andrea, bio='andreas bio')
+        response = self.client.get('/profiles/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_logged_in_user_can_create_profile(self):
+        self.client.login(username='andrea', password='rookiecoder')
+        response = self.client.post('/profiles/', {'bio': 'andreas bio'})
+        count = Profile.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_user_not_logged_in_cant_create_profile(self):
+        response = self.client.post('/profiles/', {'bio': 'andreas bio'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
