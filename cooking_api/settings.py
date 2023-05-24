62ebba205f11dd599faa06f8078ca9ff2dfdcdc4
andreagorsche <andrea.gorsche@gmail.com>
2023-05-24 20:23:46 +0000

    Debug settings.py for heroku deployment

--- cooking_api/settings.py
+++ cooking_api/settings.py
@@ -116,10 +116,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 else:
     if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+        extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+        CORS_ALLOWED_ORIGIN_REGEXES = [
+            rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+        ]
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'cooking_api.urls'
