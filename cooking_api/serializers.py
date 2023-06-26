2557afa13c65095b6ccfbabcbc177043c74c29f0
andreagorsche <andrea.gorsche@gmail.com>
2023-06-26 08:19:15 +0000

    Debug serializer of api

--- cooking_api/serializers.py
+++ cooking_api/serializers.py
@@ -2,23 +2,39 @@ from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 from profiles.models import Profile as ProfileModel
 
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
+from profiles.models import Profile as ProfileModel
 
-# from .models import userProfileModel
 
-
-class Profile(serializers.ModelSerializer):
-    user = UserDetailsSerializer()
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfileModel
-        fields = ('chef',)
+        fields = '__all__'
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile = Profile(many=True)
+    profile = ProfileSerializer(many=True)
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + ('profile',)
         read_only_fields = ('',)
+
+# from .models import userProfileModel
+
+# class Profile(serializers.ModelSerializer):
+#    user = UserDetailsSerializer()
+ #   class Meta:
+  #      model = ProfileModel
+   #     fields = ('chef',)
+
+
+#class CurrentUserSerializer(UserDetailsSerializer):
+ #   profile = Profile(many=True)
+
+  #  class Meta(UserDetailsSerializer.Meta):
+   #     fields = UserDetailsSerializer.Meta.fields + ('profile',)
+    #    read_only_fields = ('',)
 
 
 # class CurrentUserSerializer(UserDetailsSerializer):
@@ -36,3 +52,4 @@ class CurrentUserSerializer(UserDetailsSerializer):
 #     #         # 'profile_id', 'profile_image'
 #     #         'profile',
 #     #     )
+"""
