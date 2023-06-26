c22d737835a5a83969e34d320abee2f3a6399bf4
andreagorsche <andrea.gorsche@gmail.com>
2023-06-26 07:53:17 +0000

    Migrate database after debug in profile and recipe models

--- cooking_api/serializers.py
+++ cooking_api/serializers.py
@@ -1,12 +1,38 @@
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
+from profiles.models import Profile as ProfileModel
+
+
+# from .models import userProfileModel
+
+
+class Profile(serializers.ModelSerializer):
+    user = UserDetailsSerializer()
+    class Meta:
+        model = ProfileModel
+        fields = ('chef',)
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
-    profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile = Profile(many=True)
 
     class Meta(UserDetailsSerializer.Meta):
-        fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
-        )
+        fields = UserDetailsSerializer.Meta.fields + ('profile',)
+        read_only_fields = ('',)
+
+
+# class CurrentUserSerializer(UserDetailsSerializer):
+#     # profile = Profile(source='userprofile')
+#     # profile = Profile(many=True)
+#     # profile = serializers.ReadOnlyField(source='userprofile')
+#     # profile_id = serializers.ReadOnlyField(source='profile')
+#     # profile_image = serializers.ReadOnlyField(source='profile.image.url')
+#     # profile_image = serializers.ImageField(
+#     #     max_length=None, use_url=True,source='profile.image.url'
+#     # )
+
+#     # class Meta(UserDetailsSerializer.Meta):
+#     #     fields = UserDetailsSerializer.Meta.fields + (
+#     #         # 'profile_id', 'profile_image'
+#     #         'profile',
+#     #     )
--- profiles/models.py
+++ profiles/models.py
@@ -4,19 +4,16 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     chef = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(
-        upload_to='images/', default='../default_profile_uhncwe'
-    )
+    image = models.ImageField(upload_to='images/', default='../default_profile_uhncwe')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     bio = models.CharField(max_length=200, default="")
 
-class Meta:
+    class Meta:
         ordering = ['-updated_at']
 
-def __str__(self):
-    return f"{self.chef}'s profile"
-
+    def __str__(self):
+        return f"{self.chef}'s profile"
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
