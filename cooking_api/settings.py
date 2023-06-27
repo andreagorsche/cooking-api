b502d1a6b1ed10a45944afe89310b66010f37b98
andreagorsche <andrea.gorsche@gmail.com>
2023-06-27 18:36:00 +0000

    Change database settings in settings.py

--- cooking_api/settings.py
+++ cooking_api/settings.py
@@ -58,7 +58,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'False'
 
 ALLOWED_HOSTS = ['8000-andreagorsch-cookingapi-m1tec14t6l7.ws-eu101.gitpod.io', 'https://cookingapi.herokuapp.com/']
 
@@ -138,18 +138,10 @@ WSGI_APPLICATION = 'cooking_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEV' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+ 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
