217474743985a19c5b085c7cc4c2c2566a097ce7
andreagorsche <andrea.gorsche@gmail.com>
2023-10-24 13:07:44 +0000

    Add marked_inappropriate_by to the comments model

--- comments/models.py
+++ comments/models.py
@@ -7,12 +7,13 @@ class Comment(models.Model):
     """
     Comment model, related to User and Post
     """
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
     is_inappropriate = models.BooleanField(default=False)
+    marked_inappropriate_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='marked_comments')
 
     class Meta:
         ordering = ['-updated_at']
