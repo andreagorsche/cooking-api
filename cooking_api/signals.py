6c4121244d5d4e9ec15a7394ea5ae36e5551110f
andreagorsche <andrea.gorsche@gmail.com>
2023-10-25 12:13:21 +0000

    Create post-save signal with comment model as sender to count the number of inappropriate comments and set the profile inactice if inappropriate comments exceed 5

---
+++ cooking_api/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Comment, Profile
+
+@receiver(post_save, sender=Comment)
+def update_inappropriate_comments_count(sender, instance, **kwargs):
+    user = instance.owner
+    profile = Profile.objects.get(owner=user)
+
+    # Check if the comment is marked as inappropriate
+    if instance.is_inappropriate:
+        profile.inappropriate_comments_count += 1
+        profile.save()
+
+        # Check if the count exceeds 5 and set the profile as inactive
+        if profile.inappropriate_comments_count >= 5:
+            profile.is_active = False
+            profile.save()
