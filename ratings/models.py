0184b667b22b2ac080cdacc47c1fe72ba90afc4b
andreagorsche <andrea.gorsche@gmail.com>
2023-10-27 13:00:02 +0000

    Create urls.py and endpoints in ratings app, create serializers for ratings and add comment as foreign key to ratings

--- ratings/models.py
+++ ratings/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from recipes.models import Recipe
+from comments.models import Comment
+
 from django.core.validators import MaxValueValidator
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    comment = models.ForeignKey (Comment, on_delete=models.CASCADE, null=True,blank=True, default=None)
     stars = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(5)])
 
     def __str__(self):
--- ratings/views.py
+++ ratings/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from cooking_api.permissions import IsOwnerOrReadOnly
+from .models import Rating
+from .serializers import RatingSerializer, RatingDetailSerializer
 
-# Create your views here.
+class RatingList(generics.ListCreateAPIView):
+    """
+    List all ratings, if logged create your own ratings
+    """
+    serializer_class = RatingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Rating.objects.all()
+
+class RatingDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a rating, if you are logged in update or delete an owned comment
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = RatingDetailSerializer
+    queryset = Rating.objects.all()
