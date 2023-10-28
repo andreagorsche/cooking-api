8e9fbc3f854a10e940c989f6cc5d41aa7d08888c
andreagorsche <andrea.gorsche@gmail.com>
2023-10-28 19:18:13 +0000

    Add the logic that a user can only rate the same recipe once in the perform create method of the view

--- ratings/views.py
+++ ratings/views.py
@@ -16,7 +16,12 @@ class RatingList(generics.ListCreateAPIView):
         owner = self.request.user
         recipe_id = self.request.data.get('recipe')
         stars = self.request.data.get('stars')
-
+        
+        # Check if the user has already rated the recipe
+        existing_rating = Rating.objects.filter(owner=owner, recipe=recipe_id).first()
+        if existing_rating:
+            raise ValidationError("You can only rate a recipe onces. If you want to change the rating you made, please feel free to update it.")
+        
         if stars:
             recipe = Recipe.objects.exclude(owner=self.request.user).get(pk=recipe_id)  # Fetch recipe not owned by the user
             serializer.save(owner=self.request.user, recipe=recipe)
