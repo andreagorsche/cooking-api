537c0912defd6ed39887c3741e0a923252d90fa4
andreagorsche <andrea.gorsche@gmail.com>
2023-12-22 21:04:06 +0000

    Create custom urls and views for email signal

--- cooking_api/views.py
+++ cooking_api/views.py
@@ -4,7 +4,34 @@ from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
+from allauth.account.models import EmailConfirmation
+from allauth.account.signals import email_confirmed
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 
+# New signal for handling email confirmation
+@receiver(email_confirmed)
+def email_confirmed_handler(request, email_address, **kwargs):
+    user = email_address.user
+    print("Email confirmed:", user.username)
+    # Your custom logic for handling email confirmation
+
+# Custom API view for handling email confirmation
+@api_view(['GET'])
+def confirm_email(request):
+    # Extract confirmation key from the request query parameters
+    key = request.GET.get('key', '')
+    
+    # Perform the email confirmation using Allauth's utility function
+    email_confirmation = EmailConfirmation.objects.get(key=key)
+    email_confirmation.confirm()
+    
+    # Trigger your custom signal when email is confirmed
+    email_confirmed.send(sender=request, email_address=email_confirmation.email_address)
+    
+    return Response({"message": "Email confirmed successfully"})
 
 @api_view()
 def root_route(request):
