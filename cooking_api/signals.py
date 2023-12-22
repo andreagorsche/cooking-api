33aeeb1da90e3e08d9e6ab08403ba4e9423bc5d9
andreagorsche <andrea.gorsche@gmail.com>
2023-12-22 21:20:56 +0000

    Try generating an email confirmation instance and key

--- cooking_api/signals.py
+++ cooking_api/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
+from allauth.account.models import EmailConfirmation
 
 # Define the signal handler
 @receiver(post_save, sender=User)
@@ -11,8 +12,23 @@ def send_registration_email(sender, instance, created, **kwargs):
         subject = "Welcome to Cook-Around-The-Clock"
         message = "Congrats. Your registration to Cook-Around-The-Clock was successful. Jump right in and keep on cookin'."
         from_email = "andrea.gorsche@gmail.com"  
-        recipient_list = [instance.email]  # Use instance.email to get the email of the newly created user
+        recipient_list = [instance.email]
 
+        # Create EmailConfirmation instance
+        email_confirmation = EmailConfirmation.create(instance.email)
+        
+        # Customize the email confirmation
+        email_confirmation.sent = True
+        email_confirmation.save()
+
+        # Send the confirmation email
+        email_confirmation.send()
+
+        # access confirmation key in email:
+        confirmation_key = email_confirmation.key
+       
+
+        # Send the registration email
         send_mail(subject, message, from_email, recipient_list)
         
 # Connect the signal
--- cooking_api/views.py
+++ cooking_api/views.py
@@ -23,15 +23,19 @@ def email_confirmed_handler(request, email_address, **kwargs):
 def confirm_email(request):
     # Extract confirmation key from the request query parameters
     key = request.GET.get('key', '')
-    
-    # Perform the email confirmation using Allauth's utility function
-    email_confirmation = EmailConfirmation.objects.get(key=key)
-    email_confirmation.confirm()
-    
-    # Trigger your custom signal when email is confirmed
-    email_confirmed.send(sender=request, email_address=email_confirmation.email_address)
-    
-    return Response({"message": "Email confirmed successfully"})
+    print("Confirmation Key:", key)  # Add this line for debugging
+
+    try:
+        # Perform the email confirmation using Allauth's utility function
+        email_confirmation = EmailConfirmation.objects.get(key=key)
+        email_confirmation.confirm()
+        # Trigger your custom signal when email is confirmed
+        email_confirmed.send(sender=request, email_address=email_confirmation.email_address)
+        return Response({"message": "Email confirmed successfully"})
+    except EmailConfirmation.DoesNotExist:
+        print("Confirmation Key does not exist in the database")  # Add this line for debugging
+        return Response({"message": "Email confirmation failed. Invalid key."}, status=400)
+
 
 @api_view()
 def root_route(request):
