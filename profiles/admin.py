c9a6856517ea04e2730e3f6f5d2d692ae51f2de5
andreagorsche <andrea.gorsche@gmail.com>
2023-12-27 17:01:42 +0000

    Add deletion of selected profiles and according users to the admin panel by implementing the logic in the admin.py of profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Profile
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_active')
+    actions = ['delete_selected_users_and_profiles']
+
+    def delete_selected_users_and_profiles(self, request, queryset):
+        """
+        Custom action to delete selected users and their profiles.
+        """
+        for user in queryset:
+            profile = Profile.objects.get(owner=user)
+            profile.delete()
+            user.delete()
+
+        self.message_user(request, f'Selected users and their profiles have been deleted.')
+
+    delete_selected_users_and_profiles.short_description = 'Delete selected users and profiles'
+
+# Register the User and Profile models with the customized admin class
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
 admin.site.register(Profile)
