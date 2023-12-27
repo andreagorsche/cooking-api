88bce821dde5551e86b7d842ecdff252d2e21e8e
andreagorsche <andrea.gorsche@gmail.com>
2023-12-27 19:31:37 +0000

    Create a confirmation mail with django.core.mail instead of all-auth

--- cooking_api/views.py
+++ cooking_api/views.py
@@ -4,12 +4,41 @@ from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
-from allauth.account.models import EmailConfirmation
-from allauth.account.signals import email_confirmed, email_confirmation_sent, user_logged_in
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.core.mail import send_mail
 from django.contrib.auth.models import User
+from dj_rest_auth.registration.views import RegisterView
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+class CustomRegistrationView(RegisterView):
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = self.user
+
+    # Send email confirmation with customized html mail
+        email_subject = "Confirm your registration"
+        email_recipient = request.data.get('email')  # Use request.data to access form data
+
+        html_msg = render_to_string("templates/email.html")
+        plain_msg = strip_tags(html_msg)
+
+        msg = EmailMultiAlternatives(
+            subject=email_subject,
+            from_email=None,
+            to=[email_recipient]
+        )
+
+        # Attach the plain text version of the email
+        msg.attach_alternative(plain_msg, "text/plain")
+
+        # Attach the HTML version of the email
+        msg.attach(html_msg, "text/html")
+
+        msg.send()
+
 
 @api_view()
 def root_route(request):
