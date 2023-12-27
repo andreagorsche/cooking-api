35ed3cc967350e8607e59ddfe618da457b2ad844
andreagorsche <andrea.gorsche@gmail.com>
2023-12-27 18:15:08 +0000

    Delete custom registration view, not needed

--- cooking_api/urls.py
+++ cooking_api/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route), #logout route
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    # Include custom registration view from dj-rest-auth
-    path('dj-rest-auth/registration/', CustomRegistrationView.as_view(), name='dj-rest-auth-registration'),    
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),  
     path('accounts/', include('allauth.urls')),
     path('', include('profiles.urls')),
     path('', include('recipes.urls')),
