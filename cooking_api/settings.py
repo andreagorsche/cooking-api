d2411262d7c2ca715f13743103f9aa7dbfe959b4
andreagorsche <andrea.gorsche@gmail.com>
2024-01-19 20:33:25 +0000

    Whitenoise added to correct css error in django production

--- cooking_api/settings.py
+++ cooking_api/settings.py
@@ -171,6 +171,7 @@ SOCIALACCOUNT_PROVIDERS = {
 # Middleware settings
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -267,6 +268,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
