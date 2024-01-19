c543d8dbecc488df0272b3dcb2652abd5610e59b
andreagorsche <andrea.gorsche@gmail.com>
2024-01-19 21:17:38 +0000

    comment out the url pattern with the custom view

--- cooking_api/urls.py
+++ cooking_api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('dj-rest-auth/logout/', logout_route), #logout route
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     #path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('dj-rest-auth/registration/', CustomRegistrationView.as_view(), name='dj-rest-auth-registration'), 
+    #path('dj-rest-auth/registration/', CustomRegistrationView.as_view(), name='dj-rest-auth-registration'), 
     path('accounts/', include('allauth.urls')),   
     path('', include('profiles.urls')),
     path('', include('recipes.urls')),
