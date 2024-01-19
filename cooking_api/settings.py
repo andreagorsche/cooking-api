372172c440ae34444c55101eb7262dcf79c52dd0
andreagorsche <andrea.gorsche@gmail.com>
2024-01-19 21:23:32 +0000

    Remove whitenoise

--- cooking_api/settings.py
+++ cooking_api/settings.py
@@ -161,7 +161,6 @@ SOCIALACCOUNT_PROVIDERS = {
 # Middleware settings
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -258,8 +257,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
