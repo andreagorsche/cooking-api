9f37f8604900a79ad729c41501ccb98384200c7a
andreagorsche <andrea.gorsche@gmail.com>
2024-01-19 19:29:29 +0000

    Add shared folder to the directory for cuisine choices

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from shared.choices import CUISINE_CHOICES
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
---
+++ shared/cuisine.py
@@ -0,0 +1,15 @@
+CUISINE_CHOICES = (
+    ('american', 'AMERICAN'),
+    ('austrian', 'AUSTRIAN'),
+    ('caribbean', 'CARIBBEAN'),
+    ('chinese', 'CHINESE'),
+    ('french', 'FRENCH'),
+    ('german', 'GERMAN'),
+    ('greek', 'GREEK'),
+    ('indian', 'INDIAN'),
+    ('italian', 'ITALIAN'),
+    ('mediterranean', 'Mediterranean'),
+    ('mexican', 'MEXICAN'),
+    ('slovak', 'SLOVAK'),
+    ('spanish', 'SPANISH'),
+)
