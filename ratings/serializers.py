4fc7881828b8a54f3636b227b54522bddf0047e2
andreagorsche <andrea.gorsche@gmail.com>
2024-01-21 20:51:15 +0000

    change owner.username ot owner.id in serializer of ratings, change def str to owner id and recipe id too

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from django.core.validators import MinValueValidator, MaxValueValidator
-from .models import Rating, Recipe
+from .models import Rating
 
 class RatingSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    recipe_id = serializers.ReadOnlyField(source='recipe.id') 
+    owner_id = serializers.ReadOnlyField(source='owner.id')
+    recipe_id = serializers.ReadOnlyField(source='recipe.id')
     stars = serializers.IntegerField(
         validators=[
             MinValueValidator(1, message="Rating should be at least 1."),
@@ -14,16 +14,17 @@ class RatingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rating
-        fields = ['id', 'owner', 'recipe_id', 'stars']  
+        fields = ['id', 'owner_id', 'recipe_id', 'stars']
+
     def create(self, validated_data):
-        owner = self.context['request'].user
+        owner_id = validated_data['owner_id']
         recipe = validated_data['recipe']
 
-        existing_rating = Rating.objects.filter(owner=owner, recipe=recipe).first()
+        existing_rating = Rating.objects.filter(owner_id=owner_id, recipe=recipe).first()
 
         if existing_rating:
             existing_rating.stars = validated_data['stars']
             existing_rating.save()
             return existing_rating
         else:
-            return Rating.objects.create(owner=owner, **validated_data)
+            return Rating.objects.create(owner_id=owner_id, **validated_data)
