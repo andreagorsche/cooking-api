867449ab34c83c4272dea47688ba8828783ef90a
andreagorsche <andrea.gorsche@gmail.com>
2024-01-22 14:41:36 +0000

    Change rating serializer and view back to the version that last worked

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -4,7 +4,7 @@ from .models import Rating, Recipe
 
 class RatingSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    recipe_id = serializers.ReadOnlyField(source='recipe.id') 
+    recipe = serializers.ReadOnlyField() 
     stars = serializers.IntegerField(
         validators=[
             MinValueValidator(1, message="Rating should be at least 1."),
@@ -14,16 +14,5 @@ class RatingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rating
-        fields = ['id', 'owner', 'recipe_id', 'stars']  
-    def create(self, validated_data):
-        owner = self.context['request'].user
-        recipe_id = self.context['recipe_id'] 
+        fields = ['id', 'owner', 'recipe', 'stars']  
 
-        existing_rating = Rating.objects.filter(owner=owner, recipe_id=recipe_id).first()
-
-        if existing_rating:
-            existing_rating.stars = validated_data['stars']
-            existing_rating.save()
-            return existing_rating
-        else:
-            return Rating.objects.create(owner=owner, **validated_data)
--- ratings/views.py
+++ ratings/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions, filters
-from cooking_api.permissions import IsOwnerOrReadOnly
+from cooking_api.permissions import IsNotOwnerOrReadOnly
 from .models import Rating
 from recipes.models import Recipe
 from .serializers import RatingSerializer
@@ -7,35 +7,28 @@ from rest_framework.exceptions import ValidationError
 
 class RatingList(generics.ListCreateAPIView):
     serializer_class = RatingSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsNotOwnerOrReadOnly]
     
     def get_queryset(self):
         owner = self.request.user
         return Rating.objects.select_related('recipe').all() # Fetch ratings
 
-    def perform_create(self, serializer):
+     def perform_create(self, serializer):
         owner = self.request.user
-        recipe_id = serializer.validated_data.get('recipe_id')
-        stars = serializer.validated_data.get('stars')
-        
-        # Check if the user has already rated the recipe
-        existing_rating = Rating.objects.filter(owner=owner, recipe_id=recipe_id).first()
-        if existing_rating:
-            raise ValidationError("You can only rate a recipe once. If you want to change the rating you made, please feel free to update it.")
-        
+        recipe_id = self.request.data.get('recipe')
+        stars = self.request.data.get('stars')
+
         if stars:
             recipe = Recipe.objects.exclude(owner=self.request.user).get(pk=recipe_id)  # Fetch recipe not owned by the user
             serializer.save(owner=self.request.user, recipe=recipe)
         else:
             raise ValidationError("A star rating is required.")
 
-
-
         
 class RatingDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve a rating, if you are logged in update an owned rating
     """
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsNotOwnerOrReadOnly]
     serializer_class = RatingSerializer
     queryset = Rating.objects.all()
