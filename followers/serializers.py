2bb28bc9d74364814773e3ea7342662b7567f3f5
andreagorsche <andrea.gorsche@gmail.com>
2024-01-25 22:32:45 +0000

    Change followed_name to followed_id in follower serializer

--- followers/serializers.py
+++ followers/serializers.py
@@ -5,12 +5,12 @@ from .models import Follower
 
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    followed_name = serializers.ReadOnlyField(source='followed.username')
+    followed_id = serializers.ReadOnlyField(source='followed.id')
     
     class Meta:
         model = Follower
         fields = [
-            'id', 'owner', 'followed', 'created_at', 'followed_name'
+            'id', 'owner', 'followed', 'created_at', 'followed_id'
         ]
     
     def create(self, validated_data):
