e4379f02788cc9de796265b8e3630b0721fe69f5
andreagorsche <andrea.gorsche@gmail.com>
2024-01-25 22:47:00 +0000

    Change following_id to followed_id in the profiles serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -8,7 +8,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     email = serializers.ReadOnlyField(source='owner.email')
     bio = serializers.CharField(max_length=200)
     is_owner = serializers.SerializerMethodField()
-    following_id = serializers.SerializerMethodField()
+    followed_id = serializers.SerializerMethodField()
     recipes_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
@@ -31,20 +31,20 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_following_id(self, obj):
+    def get_followed_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            following = Follower.objects.filter(
+            followed = Follower.objects.filter(
                 owner=user, followed=obj.owner
             ).first()
-            return following.id if following else None
+            return followed.id if followed else None
         return None
     
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'email', 'created_at', 'updated_at', 'image', 'is_owner', 'following_id',
+            'id', 'owner', 'email', 'created_at', 'updated_at', 'image', 'is_owner', 'followed_id',
             'recipes_count', 'followers_count', 'following_count', 'inappropriate_comments_count',
             'is_active', 'bio', 'favorite_cuisine'
         ]
