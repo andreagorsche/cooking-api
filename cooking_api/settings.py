15a3606e274d62d0464b54a92ede6af58ab68b59
andreagorsche <andrea.gorsche@gmail.com>
2024-03-11 15:01:17 +0000

    Reset to regular database since testing is done

--- cooking_api/settings.py
+++ cooking_api/settings.py
@@ -206,18 +206,12 @@ WSGI_APPLICATION = 'cooking_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DEV = os.environ.get('ENABLE_LOCAL_DB', False)
+DEV = os.environ.get('ENABLE_LOCAL_DB', False)
 
-#DATABASES = {
-#            'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#    }
-    
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+            'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+    
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
