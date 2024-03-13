444706f4cf0de927ac77a6106bd816cb1331df79
andreagorsche <andrea.gorsche@gmail.com>
2024-03-13 16:06:41 +0000

    Add Verify URL and Verify email view

--- cooking_api/urls.py
+++ cooking_api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include 
-from .views import root_route, logout_route, CustomRegistrationView
+from .views import root_route, logout_route, CustomRegistrationView, verify_email
 from django.conf.urls import url
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('dj-rest-auth/logout/', logout_route), #logout route
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', CustomRegistrationView.as_view(), name='dj-rest-auth-registration'),
+    path('verify-email/<str:key>/', verify_email, name='verify_email'),
     path('accounts/', include('allauth.urls')),   
     path('', include('profiles.urls')),
     path('', include('recipes.urls')),
--- cooking_api/views.py
+++ cooking_api/views.py
@@ -4,6 +4,13 @@ from dj_rest_auth.registration.views import RegisterView
 from allauth.account.utils import send_email_confirmation
 from profiles.models import Profile
 from django.conf import settings
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from allauth.account.models import EmailAddress
+
+User = get_user_model()
 
 class CustomRegistrationView(RegisterView):
     def create(self, request, *args, **kwargs):
@@ -23,7 +30,28 @@ class CustomRegistrationView(RegisterView):
             send_email_confirmation(request, user)
 
         return response
-    
+
+
+
+def verify_email(request, key):
+    try:
+        # Retrieve the user associated with the email address
+        email_address = EmailAddress.objects.get(activation_key=key)
+        user = email_address.user
+        # Mark the email address as verified
+        email_address.verified = True
+        email_address.save()
+        # Optionally, mark the user's email as primary
+        user.email = email_address.email
+        user.save()
+        # Optionally, perform any additional actions (e.g., update user profile)
+        # Redirect to a success page or any other page
+        messages.success(request, "Email verified successfully.")
+        return HttpResponseRedirect(reverse('email_verification_success'))
+    except EmailAddress.DoesNotExist:
+        messages.error(request, "Invalid activation key.")
+        return HttpResponseRedirect(reverse('email_verification_failure'))
+
 
 @api_view()
 def root_route(request):
